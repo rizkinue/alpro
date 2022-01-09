91be3fa3b9afeb37fbdb1053d5a850f66ec834ab
Ibnu Maulana <89495996+rizkinue@users.noreply.github.com>
2022-01-10 00:25:17 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+
+////////////////////////////////////////////////////
+// PROGRAM SUPPLIER PENJUALAN TAHU MENTAH
+///////////////////////////////////////////////////
+
+
+let pendapatan, total, rata, komisi
+
+const list_jual = [[] , [], [], []]
+let	urutan		= 0 
+
+function input(){
+
+	let harga, ttl_berat
+
+	const nama	 		= document.getElementById('nm').value
+	const tanggal		= document.forms[0]["date"].value
+	const jenis			= document.forms[0]["form"].value
+	const jumlah	 	= document.forms[0]["jumlah"].value
+
+	list_jual[0][urutan] = tanggal
+	list_jual[1] 		 = jenis
+	list_jual[2][urutan] = jumlah 
+	urutan++
+
+	if(list_jual[1] == "Tahu Sumedang")
+		harga = 200
+	if(list_jual[1] == "Tahu Sutra")
+		harga = 300
+	if(list_jual[1] == "Tahu Kuning")
+		harga = 250
+	if(list_jual[1] == "Tahu Bandung")
+		harga == 500
+
+	pendapatan = harga * jumlah
+
+	komisi = pendapatan * 0.05
+
+	total = pendapatan - komisi	
+
+	if(!nama, !tanggal, !jumlah){
+		perNama.innerHTML = "Wajib Memasukkan Nama" 
+		perTgl.innerHTML = "Wajib Mengisi Tanggal "
+		perBerat.innerHTML = "Wajib Mengisi Berat Tahu"
+	} /* else if(!tanggal){
+		perTgl.innerHTML = "Wajib Mengisi Tanggal "
+
+	} else if(!jumlah){
+		perBerat.innerHTML = "Wajib Mengisi Berat Tahu"
+	} */ else{ 	
+		document.getElementById('hasil').innerHTML += `
+			<b>Nama Penjual</b> : ${nama}
+			<br> <b>Tanggal Penjualan</b> : ${tanggal}
+			<br> <b>Hasil Penjualan ${jenis}</b> : ${pendapatan}
+			<br> <b>Komisi Pabrik</b> : ${komisi}
+			<br> <b>Total Pendapatan Penjual</b> : (${pendapatan} - ${komisi}) = ${total}<hr>`
+
+			document.forms[0]["date"].value = ""
+			document.forms[0]["form"].value = ""
+    		document.forms[0]["jumlah"].value = ""
+	}
+
+}
+
